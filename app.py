eca78ba88061e8c0efd12831ba7351623e956457
Tysma12 <108798288+Tysma12@users.noreply.github.com>
2023-06-16 15:08:45 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+ 
+
+app = Flask(__name__)
+
+ 
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return """<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>page</title>
+</head>
+<body>
+<h1>C'est une page html</h1>
+</body>
+</html>"""
+
+ 
+
+
+if __name__ == '__main__':
+    app.run()
